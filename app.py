e3e70a5652b39d69295c897cd74a6dfd9934ce8f
Jose Ricardo Hernandez Hernandez <ricardohdez4567@gmail.com>
2026-06-17 11:13:42 -0600

    Agregar CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)
 
 modelo = joblib.load("modelo_autos.pkl")
 columnas = joblib.load("columnas.pkl")
