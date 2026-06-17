4c94e62ce59aeabf12447b7ac6e86aa99c8ad615
Jose Ricardo Hernandez Hernandez <ricardohdez4567@gmail.com>
2026-06-17 12:02:32 -0600

    Integrar frontend con Flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import joblib
 import pandas as pd
@@ -6,18 +6,18 @@ import pandas as pd
 app = Flask(__name__)
 CORS(app)
 
+# Cargar modelo y columnas
 modelo = joblib.load("modelo_autos.pkl")
 columnas = joblib.load("columnas.pkl")
 
 
+# Mostrar la página principal
 @app.route("/")
 def inicio():
-    return jsonify({
-        "mensaje": "API funcionando correctamente",
-        "modelo": "Random Forest"
-    })
+    return render_template("index.html")
 
 
+# Endpoint para predecir
 @app.route("/predecir", methods=["POST"])
 def predecir():
 
@@ -25,16 +25,19 @@ def predecir():
 
     df = pd.DataFrame([datos])
 
+    # Agregar columnas faltantes
     for col in columnas:
         if col not in df.columns:
             df[col] = 0
 
+    # Ordenar columnas como fueron entrenadas
     df = df[columnas]
 
+    # Predicción
     prediccion = modelo.predict(df)
 
     return jsonify({
-        "precio_estimado": float(prediccion[0])
+        "precio_estimado": round(float(prediccion[0]), 2)
     })
 
 
