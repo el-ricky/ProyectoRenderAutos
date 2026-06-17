568d082e5a11faecfdc74a2671e70d1ef60af12a
Jose Ricardo Hernandez Hernandez <ricardohdez4567@gmail.com>
2026-06-17 10:40:49 -0600

    Agregar endpoint de prediccion

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import joblib
+import pandas as pd
 
 app = Flask(__name__)
 
 modelo = joblib.load("modelo_autos.pkl")
 columnas = joblib.load("columnas.pkl")
+
 
 @app.route("/")
 def inicio():
@@ -12,6 +14,27 @@ def inicio():
         "mensaje": "API funcionando correctamente",
         "modelo": "Random Forest"
     })
+
+
+@app.route("/predecir", methods=["POST"])
+def predecir():
+
+    datos = request.get_json()
+
+    df = pd.DataFrame([datos])
+
+    for col in columnas:
+        if col not in df.columns:
+            df[col] = 0
+
+    df = df[columnas]
+
+    prediccion = modelo.predict(df)
+
+    return jsonify({
+        "precio_estimado": float(prediccion[0])
+    })
+
 
 if __name__ == "__main__":
     app.run(debug=True)
